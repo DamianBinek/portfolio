a708da8ae88a570ce71bc0f75b81431c5d8265bf
Binek <damian.binek@capgemini.com>
2019-08-30 01:35:13 +0200

    Added component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,6 @@
 <template>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
+    <v-layout text-xs-center wrap>
       <v-flex xs12>
         <p>DAMIAN.IO</p>
       </v-flex>
@@ -14,64 +11,72 @@
         </h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+          <br />please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank"
+            >Discord Community</a
+          >
         </p>
       </v-flex>
 
       <v-flex xs12>
-        <img src="../assets/placeholder-man.png" width="150px" height="150px"/>
+        <img src="../assets/placeholder-man.png" width="150px" height="150px" />
       </v-flex>
 
       <v-flex xs12>
-         <v-layout justify-center>
-    <v-divider class="divider" dark ></v-divider>
-         </v-layout>
-</v-flex>
+        <v-layout justify-center>
+          <v-divider class="divider" dark></v-divider>
+        </v-layout>
+      </v-flex>
 
-      <v-flex
-        xs12
-        mb-5
-      >
+      <v-flex xs12 mb-5>
         <v-layout justify-center>
           <a
             v-for="(link, i) in importantLinks"
             :key="i"
-            :href="link.href"
             class="subheading mx-3"
           >
-            {{ link.text }}
+            <menu-button
+              :handleClick="handleThisClick"
+              :buttonText="link.text"
+            ></menu-button>
           </a>
         </v-layout>
       </v-flex>
-
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      importantLinks: [
-        {
-          text: 'Home',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Skills',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'My Projects',
-          href: '/my-projects'
-        },
-        {
-          text: 'Contact Me',
-          href: 'https://twitter.com/vuetifyjs'
-        }
-      ]
-    })
+import MenuButton from "./MenuButton";
+
+export default {
+  components: { MenuButton },
+  data: () => ({
+    importantLinks: [
+      {
+        text: "Home",
+        href: "https://vuetifyjs.com"
+      },
+      {
+        text: "Skills",
+        href: "https://community.vuetifyjs.com"
+      },
+      {
+        text: "My Projects",
+        href: "/my-projects"
+      },
+      {
+        text: "Contact Me",
+        href: "https://twitter.com/vuetifyjs"
+      }
+    ]
+  }),
+  methods: {
+    handleThisClick: function() {
+      console.log("Dziala");
+    }
   }
+};
 </script>
 
 <style>
---
+++ src/components/MenuButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn id="menu-button" dark text="true" @click="handleClick">{{
+    buttonText
+  }}</v-btn>
+</template>
+
+<script>
+export default {
+  name: "MenuButton",
+  props: {
+    buttonText: {
+      type: String,
+      required: true
+    },
+    handleClick: {
+      type: Function,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+#menu-button {
+  background-color: transparent;
+}
+</style>
